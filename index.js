3a6a2da56a91f4222de14724389a55b2bd657db5
Arman Khanikiryan <akhanikiryan@gmail.com>
2024-05-10 18:41:57 +0400

    removed axios for tests

--- index.js
+++ index.js
@@ -48,27 +48,24 @@ app.post("/webhook",(req,res)=>{ //i want some
     if(body_param.object){
         console.log("inside body param");
         let phon_no_id=body_param.entry[0].changes[0].value.metadata.phone_number_id;
-        let from = body_param.entry[0].changes[0].value.messages[0].from;
-        let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
+        let from = body_param.entry[0].changes[0].value.statuses[0].recipient_id;
 
-        console.log("phone number "+phon_no_id);
         console.log("from "+from);
-        console.log("boady param "+msg_body);
-
-        axios({
-            method:"POST",
-            url:"https://graph.facebook.com/v13.0/"+phon_no_id+"/messages?access_token="+API_TOKEN,
-            data:{
-                messaging_product:"whatsapp",
-                to:from,
-                text:{
-                    body:"Hi.. I'm Prasath, your message is "+msg_body
-                }
-            },
-            headers:{
-                "Content-Type":"application/json"
-            }
-        });
+        console.log("phone number "+phon_no_id);
+        // axios({
+        //     method:"POST",
+        //     url:"https://graph.facebook.com/v13.0/"+phon_no_id+"/messages?access_token="+API_TOKEN,
+        //     data:{
+        //         messaging_product:"whatsapp",
+        //         to:from,
+        //         text:{
+        //             body:"Hi.. I'm Prasath, your message is "
+        //         }
+        //     },
+        //     headers:{
+        //         "Content-Type":"application/json"
+        //     }
+        // });
         res.sendStatus(200);
     }
 });
